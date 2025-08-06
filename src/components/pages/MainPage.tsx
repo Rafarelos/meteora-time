97ab611769acbd1fa3bdfecb86353d3e04e8811a
Rafarelos <rafael.marega2020@gmail.com>
2025-08-06 20:39:03 -0300

    🌟 feat: Animações MainPage

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -1,20 +1,54 @@
 import MinClockAnalog from "@/components/MinClockAnalog";
+import Typewriter from "@/components/animations/Typewriter";
+import FadeInOut from "@/components/animations/FadeInOut";
+import { useState, useEffect } from "react";
 
 type Props = {
-  aoAvancar: () => void
-}
+  aoAvancar: () => void;
+};
 
 export default function MainPage({ aoAvancar }: Props) {
+  const [showDesc, setShowDesc] = useState(false);
+  const [eraseTitle, setEraseTitle] = useState(false);
+  const [eraseDesc, setEraseDesc] = useState(false);
+  const [hideClock, setHideClock] = useState(false);
+
+  // controle animação do título
+  useEffect(() => {
+    if (!eraseTitle && !eraseDesc) {
+      const timer = setTimeout(() => setShowDesc(true), 900);
+      return () => clearTimeout(timer);
+    }
+  }, [eraseTitle, eraseDesc]);
+
+  // Controle das animações
+  function handleAvancar() {
+    setEraseDesc(true);
+    setTimeout(() => setEraseTitle(true), 500);
+    setTimeout(() => setHideClock(true), 900);
+    setTimeout(() => aoAvancar(), 1700);
+  }
+
   return (
     <div
-      onClick={aoAvancar}
-      className="flex flex-col items-center justify-center min-h-screen bg-neutral-950 cursor-pointer w-full"
+      onClick={handleAvancar}
+      className="flex flex-col items-center justify-center min-h-screen bg-neutral-950 cursor-pointer w-full select-none"
     >
-      <h1 className="text-4xl font-mono text-white mb-9">Meteora Time</h1>
-      <MinClockAnalog />
-      <p className="text-neutral-400 text-lg mt-7 font-mono">
-        Clique em qualquer lugar da tela para inicializar
-      </p>
+      <div className="mb-8 text-4xl font-mono text-white">
+        <Typewriter text="Meteora Time" erase={eraseTitle} speed={38} />
+      </div>
+      <FadeInOut show={!hideClock} className="mb-8">
+        <MinClockAnalog />
+      </FadeInOut>
+      <div className="text-neutral-400 text-lg font-mono min-h-[32px] mt-4">
+        {showDesc && (
+          <Typewriter
+            text="Clique em qualquer lugar da tela para inicializar"
+            erase={eraseDesc}
+            speed={17}
+          />
+        )}
+      </div>
     </div>
   );
 }
