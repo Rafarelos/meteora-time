d472d27ffe813356256825e3ca315bc87bc5a58b
Rafarelos <rafael.marega2020@gmail.com>
2025-08-06 04:28:29 -0300

    💅 style: ajuste tamanho botões do Dashboard ++ remoção de borda branca na seleção dos botões

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,11 +1,11 @@
-import { Globe, Timer, Clock, AlarmClock } from "lucide-react";
+import { Globe, Timer, Hourglass, AlarmClock } from "lucide-react";
 import { useState } from "react";
 import MinClockAnalogMini from "@/components/MinClockAnalogMini";
 
 let abas = [
   { value: "relogioMundial", icon: Globe },
   { value: "cronometro", icon: Timer },
-  { value: "timer", icon: Clock },
+  { value: "timer", icon: Hourglass },
   { value: "alarme", icon: AlarmClock },
 ];
 
@@ -28,18 +28,10 @@ export default function Dashboard() {
           Escolha uma funcionalidade abaixo
         </div>
       </main>
-      <footer className="w-full flex justify-around items-center py-3 bg-neutral-950 border-t border-neutral-800 fixed bottom-0 left-0 z-20">
+      <footer className="w-full flex justify-around items-center py-3 bg-neutral-950 fixed bottom-0 left-0 z-20">
         {abas.map(({ value, icon: Icon }) => (
-          <button
-            key={value}
-            onClick={() => setAbaAtiva(value)}
-            className={`flex flex-col items-center justify-center px-2 py-1 rounded transition-colors ${
-              abaAtiva === value ? "text-[#ca8a04]" : "text-neutral-500"
-            }`}
-            style={{ background: "none", border: "none" }}
-            aria-label={value}
-          >
-            <Icon size={28} strokeWidth={2.2} />
+          <button key={value} onClick={() => setAbaAtiva(value)} className={`flex flex-col items-center justify-center px-2 py-1 rounded transition-colors ${abaAtiva === value ? "text-[#ca8a04]" : "text-neutral-500"} focus:outline-none focus:ring-0`} style={{ background: "none", border: "none" }} aria-label={value}>
+            <Icon size={38} strokeWidth={1.2} />
           </button>
         ))}
       </footer>
